36ce77082488f7c602e24e6f818404a28ed29539
wswmsword <cjzjzzz@gmail.com>
2025-03-30 21:17:43 +0800

    mouse hover image move to display lens image

--- examples/breeze/src/App.jsx
+++ examples/breeze/src/App.jsx
@@ -18,7 +18,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <ImgLens />
+      <ImgLens
+        style={{ width: "300px" }}
+        src="https://images.unsplash.com/photo-1442458017215-285b83f65851?q=80&w=2448&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import styles from "./index.module.css";
 
-export default function Lens() {
-  return <>I'm Lens.</>;
+export default function Lens({ src, size = 234, scale = 2, style, className, lensStyle, lensClassName, lensProps, ...otherProps }) {
+  const [lensPos, setP] = useState("");
+  const [lensImgStyle, setS] = useState({});
+  const imgRef = useRef();
+  const originScale = useRef(1);
+  const [entered, setE] = useState(false);
+
+  return <>
+    <img ref={imgRef} src={src} onLoad={loaded} style={style} className={className}
+      onMouseMove={move}
+      onMouseEnter={enter}
+      onMouseLeave={leave}
+      {...otherProps} />
+    <div className={styles.lens} style={{ width: `${size}px`, height: `${size}px`, transform: lensPos, visibility: entered ? "visible" : "hidden" }} {...lensProps}>
+      <img src={src} style={{ ...lensImgStyle, ...lensStyle }} className={`${styles.lensImg} ${lensClassName}`} />
+    </div>
+  </>;
+
+  function move(e) {
+    const { offsetX, offsetY, clientX, clientY } = e.nativeEvent;
+    const halfLensSize = size / 2;
+    setP(`translate(${clientX - halfLensSize}px, ${clientY - halfLensSize}px)`);
+    const xPercent = Math.abs(offsetX) * 100 / imgRef.current.clientWidth;
+    const yPercent = Math.abs(offsetY) * 100 / imgRef.current.clientHeight;
+    const lensOffset = halfLensSize * originScale.current / scale;
+    setS({
+      transform: `scale(${scale / originScale.current}) translate(calc(-${xPercent}% + ${lensOffset}px), calc(-${yPercent}% + ${lensOffset}px))`
+    });
+  }
+
+  function enter() {
+    setE(true);
+  }
+
+  function leave() {
+    setE(false);
+  }
+
+  function loaded(e) {
+    const img = e.target;
+    const { naturalWidth: w, naturalHeight: h, clientWidth } = img;
+    originScale.current = w / clientWidth;
+    console.log(originScale.current)
+  }
 }
